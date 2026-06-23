5349d85a26104a524b75d54465c2f92a5a9e60c9
artemnarozenko-star <artemnarozenko@gmail.com>
2026-06-23 14:15:21 +0300

    бэкенд

---
+++ autosalon1.php
@@ -0,0 +1,5 @@
+<?session_start();
+$mysqli = new mysqli('localhost', 'root', '', 'avtosalon');
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
